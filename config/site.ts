cd1b30e445bbc1db91e4a8d561721f0d3f2dd24d
taiyebnirjhar <taiyebnirjhor@gmail.com>
2024-09-16 14:23:53 +0600

    initial commit

---
+++ config/site.ts
@@ -0,0 +1,17 @@
+export type SiteConfig = typeof siteConfig
+
+export const siteConfig = {
+  name: "PhotoForm",
+  description: "Craft Your Perfect Image.",
+  mainNav: [
+    {
+      title: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    twitter: "https://twitter.com/shadcn",
+    github: "https://github.com/shadcn/ui",
+    docs: "https://ui.shadcn.com",
+  },
+}
