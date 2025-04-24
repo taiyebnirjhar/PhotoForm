167ed2bed4db084d21c63311c1d8332d272d3463
Taiyeb Nirjhor <86164507+taiyebnirjhar@users.noreply.github.com>
2025-04-24 16:43:44 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: {
-    appDir: true,
-  },
-  output: "export",
-  distDir: "dist",
-  images: { unoptimized: true },
-  reactStrictMode: false,
-  trailingSlash: true,
+  // reactStrictMode: true,
+  // experimental: {
+  //   appDir: true,
+  // },
+  // output: "export",
+  // distDir: "dist",
+  // images: { unoptimized: true },
+  // reactStrictMode: false,
+  // trailingSlash: true,
 }
 
 export default nextConfig
