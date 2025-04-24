ad0d2743df230dcc63b2cfb4e87b157320fd9a2b
Taiyeb Nirjhor <86164507+taiyebnirjhar@users.noreply.github.com>
2025-04-24 16:46:59 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
   // },
   // output: "export",
   // distDir: "dist",
-  // images: { unoptimized: true },
+  images: { unoptimized: true },
   // reactStrictMode: false,
   // trailingSlash: true,
 }
